7c27411a1997e01f7bb520159c9bc7c661f57961
Sumit Mishra <46062583+SumitM0432@users.noreply.github.com>
2021-04-21 00:07:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 MAX_LEN = 128
 TRAIN_BATCH_SIZE = 32
 VALID_BATCH_SIZE = 16
-EPOCHS = 1
+EPOCHS = 50
 BERT_PATH = 'bert-base-uncased'
 MODEL_PATH = 'model.bin'
 TRAINING_FILE = '/home/levi/Desktop/KSE/input/train.csv'
